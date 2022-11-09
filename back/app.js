dc8fa45b7678763ff49914e016033c3766ddd80f
Alexander Buitrago <111058346+Jab1981@users.noreply.github.com>
2022-11-09 10:47:25 -0500

    usuario-password

--- back/app.js
+++ back/app.js
@@ -6,9 +6,11 @@ app.use(express.json());
 
 //importamos las rutas
 const productos = require ("./routes/products")
+const usuarios = require ("./routes/auth")
 
 //sujeto a la desicion del navegador
 app.use('/api',productos)
+app.use('/api',usuarios)
 
 //Middlware para manejar errores
 app.use(errorMiddleware)
--- back/models/auth.js
+++ back/models/auth.js
@@ -5,9 +5,48 @@ const bcryp = require ('bcryptjs')
 const usuarioSchema = new mongoose.Schema ({
     nombre:{
         type:String,
-        
-    }
-
-
-
+        required:[true, "Por favor ingrese un Nombre"],
+        maxlength:[120, "El nombre no puede exceder los 120 caracteres"]
+    },
+    email:{
+        type:String,
+        required:[120, "Por fvoringrese el correo electronico"],
+        unique:true,
+        validate:[validator.isEmail, "Por favor ingrese un email valido"]
+    },
+    password:{
+        type:String,
+        required:[true, "Por favor registre una contraseña"],
+        minlength:[8, "La contraseña no debe tenermenos de8 caracteres"],
+        select:false
+    },
+    avatar: {
+        public_id: {
+            type: String,
+            required: true
+        },
+        url: {
+            type: String,
+            required: true
+        }
+    },
+    role:{
+        type:String,
+        default:'user'
+    },
+    fechaRegistro:{
+        type:Date,
+        default:Date.now
+    },
+    resetPasswordToken:String,
+    resetPasswordExpire:Date
+  
 })
+
+usuarioSchema.pre("save",async function(next){
+    if(!this.isModified("password")){
+        next()
+    }
+    this.password = await bcryp.hash(this.password,10)
+})
+module.exports = mongoose.model('auth',usuarioSchema)
